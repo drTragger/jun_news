7ae8a0584ffcaadd84619e5ef4f33d8621f8a63b
user <user@user.com>
2020-11-09 20:22:19 +0200

    add getDBConnect

--- vendor/News.php
+++ vendor/News.php
@@ -4,4 +4,14 @@
 class News
 {
 
+
+    private function getDBConnect(){
+        $db = new mysqli(DB_HOST, DB_USER_NAME, DB_PASSWORD, DB_NAME);
+        if($db->connect_errno !=0){
+            $msg = 'connect error' . $db->connect_error;
+            die($msg);
+        }
+        return $db;
+    }
+
 }
