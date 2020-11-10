03eedbd0ac3de157cdda067303fd6e6428ecac7b
kate <ketrina113@gmail.com>
2020-11-10 20:50:46 +0200

    add item.php

--- views/pages/item.php
+++ views/pages/item.php
@@ -1,1 +1,17 @@
-<?php
+<form method="get">
+    <input type="hidden" name="page" value="all"/>
+    <input type="submit" value="All News">
+</form>
+<table>
+    <tr>
+        <td>
+            <h2><?= $_GET['title'] ?></h2>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <?= $_GET['content'] ?>
+        </td>
+    </tr>
+</table>
+
