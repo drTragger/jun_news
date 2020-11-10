c265b2a941863a1c61115f144b78295433e1022a
kate <ketrina113@gmail.com>
2020-11-10 21:17:36 +0200

    add changes

--- vendor/News.php
+++ vendor/News.php
@@ -6,28 +6,30 @@ class News
     private $news = [];
     private $db;
 
+
     public function __construct()
     {
         $this->db = new mysqli(DB_HOST, DB_USER_NAME, DB_PASSWORD, DB_NAME);
-        $this->getAllNews();
+        //$this->getAllNews();
     }
 
    public function getAllNews(){
-        $query = "SELECT * FROM 'news item';";
+        $query = "SELECT * FROM `news item`;";
         $result = $this->db->query($query);
         if($result){
             while($tmp = $result->fetch_assoc()){
                 $this->news[] = $tmp;
             }
         }
+        var_dump($this->news);
         return $this->news;
     }
 
     public function addNewsItem($newNews){ //TODO check DB
         $title = $newNews['title'];
         $oneItem = $newNews['content'];
-        $query = "INSERT INTO 'news item' (title, text) VALUES ('$title','$oneItem');";
-        //header('Location:' . $_SERVER['PHP_SELF']);  //TODO make redirect
+        $query = "INSERT INTO `news item` (id, title, text) VALUES (NULL, '$title', '$oneItem');";
+        //Router::redirect($_SERVER['PHP_SELF']);
         return $this->db->query($query);
     }
 
