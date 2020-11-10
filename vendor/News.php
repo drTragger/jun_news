1901e832dd1d6f659c35e31d45180b8a9e4b3f25
kate <ketrina113@gmail.com>
2020-11-10 20:49:24 +0200

    changes in News.php

--- vendor/News.php
+++ vendor/News.php
@@ -3,23 +3,17 @@
 
 class News
 {
-    public $news;
-    public $title;
-    public $content;
-    //private $news = [];
+    private $news = [];
     private $db;
-    private $oneitem;
-
 
     public function __construct()
     {
         $this->db = new mysqli(DB_HOST, DB_USER_NAME, DB_PASSWORD, DB_NAME);
-
+        $this->getAllNews();
     }
 
    public function getAllNews(){
-
-        $query = "SELECT * FROM news item;";
+        $query = "SELECT * FROM 'news item';";
         $result = $this->db->query($query);
         if($result){
             while($tmp = $result->fetch_assoc()){
@@ -29,11 +23,17 @@ class News
         return $this->news;
     }
 
-    public function addNewsItem($oneitem){
-//        $query =
-//        redirect($_SERVER['PHP_SELF']);
-//        return $db->query($query);
+    public function addNewsItem($newNews){ //TODO check DB
+        $title = $newNews['title'];
+        $oneItem = $newNews['content'];
+        $query = "INSERT INTO 'news item' (title, text) VALUES ('$title','$oneItem');";
+        //header('Location:' . $_SERVER['PHP_SELF']);  //TODO make redirect
+        return $this->db->query($query);
     }
 
+    public function getNewsItem($id){   //check
+        $query = "SELECT * FROM `news item` WHERE id = $id";
+        return $this->db->query($query);
+    }
 
 }
